25858c95ac11f3c058d23a70c38fc1a93bed10ec
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-03-05 21:55:28 +0300

    [feat] add a 404 page

---
+++ src/routes/[...path]/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import AppFooter from '$lib/components/app-footer.svelte';
+</script>
+
+<section class="flex h-full w-full flex-col items-center justify-center gap-2">
+	<h1 class="text-2xl text-shadow-destructive">Error</h1>
+	<p class="text-destructive">{page.error?.message ?? 'Something went wrong'}</p>
+</section>
+<AppFooter />
